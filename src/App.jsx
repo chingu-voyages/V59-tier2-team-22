fa7af0015cf323f8e1b3c3a84205d797aec61498
Pietrols <kabambapeter3@gmail.com>
2026-01-26 01:02:09 +0200

    feat: add routes to pages, with layout as outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import RolesPage from "./pages/RolesPage";
+import QuestionsPage from "./pages/QuestionsPage";
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="roles" element={<RolesPage />} />
+          <Route path="questions/:role" element={<QuestionsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
