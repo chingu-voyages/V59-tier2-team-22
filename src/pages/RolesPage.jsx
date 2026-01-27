6ba1bb13ae0ddaef487e489bbcf129a78c4c3bf1
abbey eleazer <abbeyeleazer@gmail.com>
2026-01-27 02:06:33 +0000

    link role card to question page

--- src/pages/RolesPage.jsx
+++ src/pages/RolesPage.jsx
@@ -1,19 +1,17 @@
 import { Link } from "react-router";
 import RoleCard from "../components/cards/RoleCard";
-import  roles  from "../data/jobRoles.js";
+import roles from "../data/jobRoles.js";
 
 const rolesData = roles;
 
 function RolesPage() {
-  
   return (
     <>
-      <Link to="/questions/:role">Questions</Link>
       <p>
         <Link to="/">Home</Link>
       </p>
       <section className="roles-page-container">
-        <div className="min-w-[20rem] md:w-[40rem] text-center mb-8 ">
+        <div className="min-w-[20rem] md:w-160 text-center mb-8 ">
           <h2>Find Your Job Role</h2>
           <p className="mt-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
@@ -36,7 +34,9 @@ function RolesPage() {
         <div className="mt-12 w-full border border-gray-300 rounded-3xl p-6 ">
           <ul className="role-cards-flex">
             {rolesData.map((role) => (
-              <RoleCard key={role} role={role} />
+              <Link to={`/questions/${role.title}`} key={role.title}>
+                <RoleCard key={roles.title} role={role} />
+              </Link>
             ))}
           </ul>
         </div>
