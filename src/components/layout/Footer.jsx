efc5d627c551f1eb2d442e38929b699639f00e92
Nandhini <nandy2991@gmail.com>
2026-02-01 11:10:02 -0600

    added team details to footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,50 @@
 function Footer() {
   return (
     <>
-      <div>
-        <p>footer</p>
-        2026 Interview Prep Team
-      </div>
+      <footer className="bg-gray-900 text-gray-300 py-2 px-2">
+        <div className="max-w-6xl mx-auto">
+        <h4 className="text-center">Interview Question Generator</h4>
+        
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mb-2">
+          <li>
+          <p>Ogechi Ogharandukun</p>
+          <div>
+            <a href= "https://github.com/Mide02-Project">Github</a>
+            <a href= "http://www.linkedin.com/in/Ogechi-Ogharandukun">Linkedln</a>
+          </div>
+          
+          </li>
+          <li>
+          <p>Peter Kabamba</p>
+          <a href= "https://github.com/Pietrols">Github</a>
+          <a href= "https://linkedin.com/in/peter-kabamba-959a061b9">Linkedln</a>
+          </li>
+          <li>
+          <p>Lawrence Narh-Lawson</p>
+          <a href= "https://github.com/lawlawson">Github</a>
+          <a href= "https://www.linkedin.com/in/lawlawson/">Linkedln</a>
+          </li>
+          <li>
+          <p>Eleazer Abbey </p>
+          <a href= "https://github.com/abbey-eleazer">Github</a>
+          <a href= "https://www.linkedin.com/in/eleazer-abbey/">Linkedln</a>
+          </li>
+          <li>
+          <p>Nandhini Ravichandran </p>
+          <a href= "https://github.com/Nandhini0123">Github</a>
+          <a href= "https://www.linkedin.com/in/nan-ravi/">Linkedln</a>
+          </li>
+          <li>
+          <p>Sophie Nora Keil </p>
+          <a href= "https://github.com/zofienora">Github</a>
+          <a href= "https://www.linkedin.com/in/sophie-nora-keil/">Linkedln</a>
+          </li>
+        </ul>
+        <div className="text-center">
+        <a href= "https://github.com/chingu-voyages/V59-tier2-team-22">© copyright 2026</a>
+        </div>
+        </div>
+      </footer>
     </>
   );
 }
