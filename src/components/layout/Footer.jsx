50965a6d1ddc9660487f34f1ee0f0b7d3dfcf760
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2026-02-07 18:59:45 +0000

    updated footeer with link

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,15 @@
 function Footer() {
   return (
-    <div className="container mx-auto px-4 py-6 text-center text-gray-600">
-      <p className="text-sm">
-        © 2026 Interview Prep Team. All rights reserved.
+    <div className='container mx-auto px-4 py-6 text-center text-gray-600'>
+      <p className='text-sm'>
+        © 2026 Interview Prep Team. All rights reserved. |{' '}
+        <a
+          href='https://github.com/chingu-voyages/V59-tier2-team-22'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-yellow-400 hover:text-yellow-500 transition-colors duration-200'>
+          GitHub
+        </a>
       </p>
     </div>
   );
