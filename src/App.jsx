aa2a1ecb9aa06ad2028c9c85cedeea5c95127001
Peter Kabamba <kabambapeter3@gmail.com>
2026-02-12 22:35:10 +0200

    Revert "Feature/additional pages"

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
-import { InterviewProvider } from './context/InterviewContext';
-import { AuthProvider } from './context/AuthContext';
-import Layout from './components/layout/Layout';
-import HomePage from './pages/HomePage';
-import RolesPage from './pages/RolesPage';
-import QuestionsPage from './pages/QuestionsPage';
-import SummaryPage from './pages/SummaryPage';
-import FAQsPage from './pages/FAQsPage';
-import ContactUsPage from './pages/ContactUsPage';
+import { BrowserRouter, Routes, Route } from "react-router";
+import { InterviewProvider } from "./context/InterviewContext";
+import { AuthProvider } from "./context/AuthContext";
+import { ChatProvider } from "./context/ChatContext";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import RolesPage from "./pages/RolesPage";
+import QuestionsPage from "./pages/QuestionsPage";
+import SummaryPage from "./pages/SummaryPage";
 
 function App() {
   return (
     <AuthProvider>
       <InterviewProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path='roles' element={<RolesPage />} />
-              <Route path='faqs' element={<FAQsPage />} />
-              <Route path='contact-us' element={<ContactUsPage />} />
-              <Route path='summary' element={<SummaryPage />} />
-              <Route path='questions/:role' element={<QuestionsPage />} />
-            </Route>
-          </Routes>
+          <ChatProvider>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
+                <Route path="roles" element={<RolesPage />} />
+                <Route path="summary" element={<SummaryPage />} />
+                <Route path="questions/:role" element={<QuestionsPage />} />
+              </Route>
+            </Routes>
+          </ChatProvider>
         </BrowserRouter>
       </InterviewProvider>
     </AuthProvider>
